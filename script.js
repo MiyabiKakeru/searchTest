c28895d6d7e3c80fa83790323fa5f474a832c961
MiyabiKakeru <masato.20110728@gmail.com>
2026-02-15 20:02:46 +0900

    Refactor button click and input event handling
    Refactor event listeners for button and input to update search URL dynamically.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let submitted = false;
-  document.getElementById('hidden_iframe').addEventListener('load', function() {
-    if (submitted) {
-      window.location.href = "https://miyabikakeru.github.io/searchtest/redirect/redirect.html";
-    }
-  });
-document.querySelector("button").addEventListener("click", () =>  {
+const button = document.querySelector("button")
+const input = document.querySelector("input")
+input.addEventListener("input", () => {
+    button.setAttribute("onclick","https://www.google.com/search?q=" + input.value)
+})
+button.addEventListener("click", () => {
     location.href = `https://www.google.com/search?q=${document.querySelector("input").value}`;
 })
